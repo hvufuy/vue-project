ba108f8317396a95e17b259f9949731622744eef
hvufuy <BJT707146093@outlook.com>
2023-10-17 20:07:24 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/vue-project',
+  base: '/vue-project/',
   build: {
     outDir: 'docs'
   }
